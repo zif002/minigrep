9ac7cd29428cbf1895d391ed0a17806e0b7578ee
Jacob <jacob.zhik@gmail.com>
2024-05-10 23:27:33 +0300

    add read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+    let content = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("Width text:\n{content}");
 }
