7715e9d6888f627155510b5951bacadffce7640c
Jacob <jacob.zhik@gmail.com>
2024-07-07 22:36:38 +0300

    finish minigrep

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     if let Err(e) = minigrep::run(config) {
-        println!("Applicatin error: {e}");
+        eprintln!("Applicatin error: {e}");
         process::exit(1);
     }
 }
